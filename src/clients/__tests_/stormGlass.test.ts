5621ccbb8a70836bac9e6656a8ba59afe22926c4
Darlan Jr <kelgaxd@gmail.com>
2023-11-13 01:02:38 -0300

    update

---
+++ src/clients/__tests_/stormGlass.test.ts
@@ -0,0 +1,15 @@
+import { StormGlass } from '@src/clients/stormGlass'
+import axios from 'axios'
+
+describe('StormGlass client', () => {
+  it('should return the normalized forecast fromt the StormGlass service', async () => {
+    const lat = -33.792726
+    const lng = 151.289824
+
+    axios.get = jest.fn().mockResolvedValue({})
+
+    const stormGlass = new StormGlass(axios)
+    const response = await stormGlass.fetchPoints(lat, lng)
+    expect(response).toEqual({})
+  })
+})
